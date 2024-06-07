c6d183a9c108a118eba60d3fb0ab63b7134a8e2c
tk yang <takijaki516@gmail.com>
2024-06-08 07:59:17 +0900

    prisma + nextauth

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
-import { ReadonlyURLSearchParams } from "next/navigation";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export const createUrl = (
-  pathname: string,
-  params: URLSearchParams | ReadonlyURLSearchParams,
-) => {
-  const paramsString = params.toString(); // REVIEW:
-  const queryString = `${paramsString.length ? "?" : ""}${paramsString}`;
-  return `${pathname}${queryString}`;
-};
-
-export const ensureStartsWith = (stringToCheck: string, startsWith: string) => {
-  stringToCheck.startsWith(startsWith)
-    ? stringToCheck
-    : `${startsWith}${stringToCheck}`;
-};
-
-export const validateEnvironmentVariables = () => {};
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
