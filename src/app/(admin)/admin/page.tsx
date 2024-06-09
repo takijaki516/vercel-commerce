3d0c4c6466ad26aed2b3d532f7534bc2ff2b961c
tk yang <takijaki516@gmail.com>
2024-06-10 02:07:57 +0900

    admin page

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,10 +1,3 @@
-import { auth } from "@/auth";
-
 export default async function AdminPage() {
-  const session = await auth();
-  if (!session || !session.admin) {
-    return null;
-  }
-
   return <div>ADMIN PAGE</div>;
 }
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,7 +1,31 @@
-export default function AdminLayout({
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import Link from "next/link";
+
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (!session) {
+    return (
+      <div>
+        <Link href="/'">GO TO HOME</Link>
+        <div className="text-2xl">Not logged in</div>
+      </div>
+    );
+  }
+
+  if (!session.admin) {
+    return (
+      <div>
+        <Link href="/'">GO TO HOME</Link>
+        <div className="text-2xl">Not an admin</div>
+      </div>
+    );
+  }
+
   return <div>{children}</div>;
 }
